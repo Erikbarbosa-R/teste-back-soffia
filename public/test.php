0e686c5869b1218217f2f0ac75dcd3087f7d4585
ERIK BARBOSA <135337607+Erikbarbosa-R@users.noreply.github.com>
2025-09-13 14:31:54 -0300

    fix: simplificar arquivo test.php e adicionar index.html

--- public/test.php
+++ public/test.php
@@ -1,5 +1,1 @@
-<?php
-// Arquivo simples para testar se o servidor está funcionando
-header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'message' => 'Server is running']);
-?>
+<?php echo 'OK'; ?>
